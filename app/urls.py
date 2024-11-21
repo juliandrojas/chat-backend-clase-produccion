6d00804aa57e24d8338d044123077e13b415057e
Julian Rojas <juliandra140201@gmail.com>
2024-11-21 15:50:55 -0500

    "Updated 'update-profile-picture' URL pattern to include '/profile/'"

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('messages/', view=views.get_messages, name='get-messages'),
     path('messages/create/', view=views.create_message, name='create-message'),
-    path('authors/<int:author_id>/', view=views.update_profile_picture, name='update-profile-picture'),
+    path('authors/<int:author_id>/profile/', view=views.update_profile_picture, name='update-profile-picture'),
     path('authors/<str:username>/', view=views.get_author_by_username, name='get-author-by-username'),
 ]
