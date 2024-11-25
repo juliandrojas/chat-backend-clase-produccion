c5efa7452d3750c126efd38a94c80135b492f673
Julian Rojas <juliandra140201@gmail.com>
2024-11-25 18:47:42 -0500

    Fix("Views of urls")

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('messages/', view=views.get_messages, name='get-messages'),
-    path('messages/create/', view=views.create_message, name='create-message'),
-    path('authors/<int:author_id>/profile/', view=views.update_profile_picture, name='update-profile-picture'),
-    path('authors/<str:username>/', view=views.get_author_by_username, name='get-author-by-username'),
+    path('messages/', views.get_messages, name='get-messages'),
+    path('messages/create/', views.create_message, name='create-message'),
+    path('authors/<int:author_id>/profile_picture/', views.update_profile_picture, name='update-profile-picture'),
+    path('authors/<str:username>/', views.get_author_by_username, name='get-author-by-username'),
 ]
